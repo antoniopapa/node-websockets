84d496d17e109553714505ecac0008d29741e837
Antonio Papa <antoniopapa1991@gmail.com>
2024-04-03 21:35:57 +0200

    5-Typeorm

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,33 @@ import express from 'express';
 import cors from 'cors';
 import {routes} from "./routes";
 import './socket';
+import {DataSource} from "typeorm";
 
-const app = express()
+const dataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "rootroot",
+    database: "node_websockets",
+    entities: [],
+    logging: false,
+    synchronize: true
+});
 
-app.use(express.json())
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5000']
-}))
+dataSource.initialize().then(connection => {
+    const app = express()
 
-routes(app);
+    app.use(express.json())
+    app.use(cors({
+        origin: ['http://localhost:3000', 'http://localhost:4200', 'http://localhost:5000']
+    }))
 
-app.listen(8000, () => {
-    console.log('listening to port 8000')
+    routes(app);
+
+    app.listen(8000, () => {
+        console.log('listening to port 8000')
+    })
 })
+
+
