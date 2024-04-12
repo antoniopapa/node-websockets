0488b7ca30fbfc2ab6d854e66a530d19419c43a9
Antonio Papa <antoniopapa1991@gmail.com>
2024-04-12 19:57:34 +0200

    14-Messages

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -1,7 +1,16 @@
 import {io} from '../socket'
+import {Message} from "../entities/message.entity";
 
-export const Message = (req, res) => {
-    io.emit("message", req.body.message);
-    console.log(req.body.message)
+export const SendMessage = async (req, res) => {
+    const user = req["user"]
+
+    const message = await Message.save({
+        sender: user,
+        receiver: {id: req.body.receiver_id},
+        content: req.body.content
+    })
+
+    io.emit("message", message.content)
+
     res.send('success');
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,15 @@
 import {Router} from "express";
-import {Message} from "./controllers/message.controller";
+import {SendMessage} from "./controllers/message.controller";
 import {GetUser, Login, Register, UpdateUser} from "./controllers/auth.controller";
 import {AuthMiddleware} from "./middlewares/auth.middleware";
 import {Users} from "./controllers/user.controller";
 
 
 export const routes = (router: Router) => {
-    router.post('/api/message', Message)
     router.post('/api/register', Register)
     router.post('/api/login', Login)
     router.get('/api/user', AuthMiddleware, GetUser)
     router.put('/api/user', AuthMiddleware, UpdateUser)
     router.get('/api/users', AuthMiddleware, Users)
+    router.post('/api/messages', AuthMiddleware, SendMessage)
 }
