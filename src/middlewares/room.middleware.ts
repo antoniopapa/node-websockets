f5fa1aadd4c6bae2f8ad55b5576c8d21db5856e5
Antonio Papa <antoniopapa1991@gmail.com>
2024-05-05 17:25:51 +0200

    21-Connect Messages with Rooms

--- src/middlewares/room.middleware.ts
+++ src/middlewares/room.middleware.ts
@@ -3,6 +3,6 @@ import {Room} from "../entities/room.entity";
 
 export const RoomMiddleware = async (req: Request, res: Response, next: Function) => {
     const id = req.params.room as any
-    req['room'] = await Room.findOne({where: {id}})
+    req['room'] = await Room.findOne({where: {id}, relations: ["members"]})
     next()
 }
